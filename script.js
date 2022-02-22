b4f233e9b7eb7f6cecad8047a6ecc01cbd0b79f2
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2022-02-22 13:38:42 +0100

    added await async

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ let previous = document.querySelector("#previous");
 let pageNumber = 1;
 let buttonclick = 0;
 let elementNo = document.querySelector("#resultgrid");
-
+let fixedColor = inputColor.value
 let form = document.querySelector('form');
+
+
 
 form.onsubmit = event => {
     event.preventDefault();
 }
 
-exeButton.onclick = function (event) {
+exeButton.onclick = async function (event) {
     document.querySelector("#resultgrid").innerHTML = ""
     buttonclick++;
     pageNumber = 1;
-    SearchPhotos();
-
+    let data = SearchPhotos();
+    ManipulateDOM(await data);
 }
-
-function SearchPhotos() {
+async function SearchPhotos() {
     let fetchUrl = "";
-    let fixedColor = inputColor.value
     if (inputColor.value == "anycolor") {
         let params = new URLSearchParams({
             key: "25706674-b9c01a86dee6bf80ba5a5b48f",
@@ -32,6 +32,8 @@ function SearchPhotos() {
             page: pageNumber,
         });
         fetchUrl = "https://pixabay.com/api/?" + params.toString();
+        let response = await fetch(fetchUrl);
+        return response.json();
     }
     else {
         let params = new URLSearchParams({
@@ -42,56 +44,53 @@ function SearchPhotos() {
             page: pageNumber,
         });
         fetchUrl = "https://pixabay.com/api/?" + params.toString();
+        let response = await fetch(fetchUrl);
+        return response.json();
     }
-
-    fetch(fetchUrl)
-        .then(function (data) {
-            return data.json();
-        })
-        .then(function (data) {
-            if (data.totalHits == 0) {
-                const thisTitle = document.createElement("li");
-                thisTitle.innerHTML = "No results found";
-                document.querySelector("#resultgrid").appendChild(thisTitle)
+}
+async function ManipulateDOM(data){
+    if (data.totalHits == 0) {
+        const thisTitle = document.createElement("li");
+        thisTitle.innerHTML = "No results found";
+        document.querySelector("#resultgrid").appendChild(thisTitle)
+        return;
+    }
+    let dataList = data.hits;
+    for (let obj of dataList){
+        const thisTitle = document.createElement("li");
+        thisTitle.innerHTML = "<a href=" + obj.largeImageURL + "><img src=" + obj.webformatURL + "></img></a><p>Author: " + obj.user + "</p><p>Tags: " + obj.tags + "</p>";
+        document.querySelector("#resultgrid").appendChild(thisTitle)
+    }
+    let maxPage = elementNo.childElementCount;
+    next.onclick = async function (event) {
+        if (maxPage == 10) {
+            if (buttonclick == 0) {
                 return;
             }
-            data.hits.forEach(obj => {
-                const thisTitle = document.createElement("li");
-                thisTitle.innerHTML = "<a href=" + obj.largeImageURL + "><img src=" + obj.webformatURL + "></img></a><p>Author: " + obj.user + "</p><p>Tags: " + obj.tags + "</p>";
-                document.querySelector("#resultgrid").appendChild(thisTitle)
-            });
-            let maxPage = elementNo.childElementCount;
-            next.onclick = function (event) {
-                if (maxPage == 10) {
-                    if (buttonclick == 0) {
-                        return;
-                    }
-                    else {
-                        if (fixedColor != inputColor.value) {
-                            inputColor.value = fixedColor;
-                        }
-                        pageNumber++;
-                        document.querySelector("#resultgrid").innerHTML = ""
-                        SearchPhotos();
-                    }
-                }
-                else {
-                    return;
-                }
-            }
-            previous.onclick = function (event) {
-                if (pageNumber == 1) {
-                    return;
-                }
-                else {
-                    if (fixedColor != inputColor.value) {
+            else {
+                if (fixedColor != inputColor.value) {
                         inputColor.value = fixedColor;
-                    }
-                    pageNumber--;
-                    document.querySelector("#resultgrid").innerHTML = ""
-                    SearchPhotos();
                 }
-
+                pageNumber++;
+                document.querySelector("#resultgrid").innerHTML = ""
             }
-        });
-}
+        }
+        else {
+            return;
+        }
+    }
+    previous.onclick = async function (event) {
+        if (pageNumber == 1) {
+            return;
+        }
+        else {
+            if (fixedColor != inputColor.value) {
+                inputColor.value = fixedColor;
+            }
+            pageNumber--;
+            document.querySelector("#resultgrid").innerHTML = ""
+            let data = SearchPhotos();
+            ManipulateDOM(await data);
+        }
+    }
+};
